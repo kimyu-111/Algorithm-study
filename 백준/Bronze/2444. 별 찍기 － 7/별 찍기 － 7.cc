0622293be6a445ca02315eef12fb059178a766c0
Yu kim <kimyou3000@naver.com>
2026-02-10 23:37:18 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        
+        for (int j = 0; j < N - i; j++) {
+            printf(" ");
+        }
+       
+        for (int j = 0; j < 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    
+    for (int i = N - 1; i >= 1; i--) {
+       
+        for (int j = 0; j < N - i; j++) {
+            printf(" ");
+        }
+      
+        for (int j = 0; j < 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
